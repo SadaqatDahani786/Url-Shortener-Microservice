f98c73d530d00734801d8e312b375c2ab8f1258f
Sadaqat Dahani <sadaqat.dahani2013@gmail.com>
2022-11-13 23:09:11 +0500

    Created view index.html and index route for it.

--- app.js
+++ app.js
@@ -20,6 +20,21 @@ const PORT = 3000;
  ** **
  */
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(express.static("public"));
+
+/*
+ ** **
+ ** ** ** ROUTES
+ ** **
+ */
+/*
+ ** ======================================
+ ** Route Index
+ ** ======================================
+ */
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 /*
  ** **
